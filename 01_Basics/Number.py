468c8daacb1da9d6b08df49a36ae261a0bb95b50
Sakin790 <sakinislam79@gmail.com>
2024-02-25 23:28:46 +0600

    fix all bug

---
+++ 01_Basics/Number.py
@@ -0,0 +1,20 @@
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x) # 1.0
+
+#convert from float to int:
+b = int(y)  # 2
+
+#convert from int to complex:
+c = complex(x) (1+0j)
+
+print(a) # 1.0 
+print(b) # 2
+print(c) # (1+0j)
+
+print(type(a)) # <class 'float'>
+print(type(b)) # <class 'int'>
+print(type(c)) #<class 'complex'>
