a997653f5b9541aba3fb5a7c78815ade6b093801
Sakin790 <sakinislam79@gmail.com>
2024-02-25 19:24:10 +0600

    DataType end

--- 01_Basics/DataType.py
+++ 01_Basics/DataType.py
@@ -1,6 +1,51 @@
 x = int (10);
 y = str ("sakin");
-
-
 print(type (x));
 print(type (y));
+
+
+a, b, c = "Orange", "Banana", "Cherry"
+print(a)
+print(b)
+print(c)
+
+
+name1 , name2, name3 = "Sakin", "Mahid", "Abir"
+print(name1,name2,name3)
+
+
+score1 = score2 = score3 = 100
+print(score1,score2,score3)
+
+# Unpack a Collection
+fruits = ["apple", "banana", "cherry"]
+fruit1,fruit2,fruit3 = fruits
+print(fruit1,fruit2,fruit3)
+
+#Assignment 
+count1 = 100
+count2=200
+print(count1+count2) #300
+print(count1,count2)# 100 200
+
+
+# Global Variables
+"""
+Variables that are created outside of a function (as in all of the examples above) are known as global variables.
+Global variables can be used by everyone, both inside of functions and outside.
+"""
+
+item="I LOVE BANGLADESH" #Global Scope
+def myFun():
+    item = "I LOVE BAN"  #Function Scope
+    print(item)
+myFun()
+
+print(item)
+ 
+ # Global 
+def myFunction():
+    global z # z is now a Global Variable
+    z = "I'm the Global Scope"
+myFunction() 
+print(z)      
