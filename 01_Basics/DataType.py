1f70e8370726e8fe2232cea54998cee32c194632
Sakin790 <sakinislam79@gmail.com>
2024-02-25 20:03:35 +0600

    dataType

--- 01_Basics/DataType.py
+++ 01_Basics/DataType.py
@@ -1,53 +1,18 @@
-# In Python I don`t to Define type , but if i want then i can
-
-x = int (10);
-y = str ("sakin");
-print(type (x));
-print(type (y));
-
-
-a, b, c = "Orange", "Banana", "Cherry"
-print(a)
-print(b)
-print(c)
-
-
-name1 , name2, name3 = "Sakin", "Mahid", "Abir"
-print(name1,name2,name3)
-
-
-score1 = score2 = score3 = 100
-print(score1,score2,score3)
-
-# Unpack a Collection
-fruits = ["apple", "banana", "cherry"]
-fruit1,fruit2,fruit3 = fruits
-print(fruit1,fruit2,fruit3)
-
-#Assignment  Operator
-count1 = 100
-count2=200
-print(count1+count2) #300
-print(count1,count2)# 100 200
-
-
-# Global Variables
 """
-Variables that are created outside of a function (as in all of the examples above) are known as global variables.
-Global variables can be used by everyone, both inside of functions and outside.
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
 """
 
-item="I LOVE BANGLADESH" #Global Scope
-def myFun():
-    item = "I LOVE BAN"  #Function Scope
-    print(item)
-myFun()
+# int/PurnoShongha
+x= 10
+print(type(x))  #<class 'int'>
 
-print(item)
- 
- # Global variable De-clearation
-def myFunction():
-    global z # z is now a Global Variable
-    z = "I'm the Global Scope"
-myFunction() 
-print(z)      
+# String
+name= "Mahid"
+print(type(name)) #<class 'str'>
--- README.md
+++ README.md
@@ -22,3 +22,11 @@
 - Camel Case  myVariableName = "John"
 - Pascal Case MyVariableName = "John"
 - Snake Case my_variable_name = "John"
+- Text Type:  =>	str
+- Numeric Types: =>	int, float, complex
+- Sequence Types: =>	list, tuple, range
+- Mapping Type: =>	dict
+- Set Types:  =>	set, frozenset
+- Boolean Type: =>	True(1)/False(0)
+- Binary Types:	 => bytes, bytearray, memoryview
+- None Type:  =>	NoneType
