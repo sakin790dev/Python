f67988fe189deeb412453d2dc0a1f9f2ec1c7952
Sakin790 <sakinislam79@gmail.com>
2024-02-25 20:34:01 +0600

    DataType covered

--- 01_Basics/DataType.py
+++ 01_Basics/DataType.py
@@ -2,9 +2,9 @@
 Text Type:	str
 Numeric Types:	int, float, complex
 Sequence Types:	list, tuple, range
-Mapping Type:	dict
+Mapping Type:	dictionary
 Set Types:	set, frozenset
-Boolean Type:	bool
+Boolean Type:	True / False
 Binary Types:	bytes, bytearray, memoryview
 None Type:	NoneType
 """
@@ -16,3 +16,38 @@ print(type(x))  #<class 'int'>
 # String
 name= "Mahid"
 print(type(name)) #<class 'str'>
+
+#Float
+floatNumber= 20.5
+print(type(floatNumber)) # <class 'float'>
+
+# Complex number
+complexNumber = 5+6j
+sum = complexNumber * 2
+print(sum) # (10+12j)
+
+#List
+list = ["apple", "banana", "cherry"] 
+"""
+List:
+1.Lists are a built-in data structure in Python.
+2.They can hold elements of different data types.
+3.Lists are dynamic and resizable, meaning you can add, remove, or modify elements freely.
+4.Lists support a variety of methods for manipulation such as append(), remove(), pop(), etc.
+5.Lists are part of the standard Python library (list type) and are widely used for general-purpose data storage and manipulation.
+"""
+
+# Tuple
+x = ("apple", "banana", "cherry")
+# We will learn more in future
+print(x[1]) # via index
+
+# Set
+x = {"apple", "banana", "cherry"} # 2nd Bracket
+
+#Dictionary
+x = {"name" : "John", "age" : 36} # key value pair
+
+#frozenset 
+tuple + set
+x = frozenset({"apple", "banana", "cherry"})
