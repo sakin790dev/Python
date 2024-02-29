8c914222ff2e1129382365e9d1427e314e45faef
Sakin790 <sakinislam79@gmail.com>
2024-02-29 19:39:09 +0600

    loop

---
+++ 03_List/LoopInList.py
@@ -0,0 +1,29 @@
+name= ["Sakin", "Tamim","Abir"]
+for x in name:
+        print(x)
+
+
+mix= [True, 12,"Score"]
+for y in mix:
+        print(y)
+        
+# Via index   
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+  
+
+
+
+score = [100, 200, 300, 400]
+length= len(score) # 4
+i = 0
+while i < length:
+        print(score[i])
+        i= i+1
+        
+"""
+i have an list named "score", 
+1, i is 0
+2,totokhon loop cholbe jotokhno na 
+"""
--- DryRun.py
+++
@@ -1,4 +0,0 @@
-thislist = ["apple", "banana", "cherry"]
-thistuple = ("kiwi", "orange")
-thislist.extend(thistuple)
-print(thislist)
